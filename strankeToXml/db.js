ff123b370bef933269d12de2b44b715e2768272f
Simon Rakovic <simonrakovic@gmail.com>
2018-06-13 00:10:15 +0200

    fixes

---
+++ strankeToXml/db.js
@@ -0,0 +1,42 @@
+const {DB_URI} = require('./../constants')
+
+// Bring Mongoose into the app
+var mongoose = require( 'mongoose' );
+
+// Build the connection string
+
+
+// Create the database connection
+mongoose.connect(DB_URI);
+
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose default connection open to ' + DB_URI);
+});
+
+// If the connection throws an error
+mongoose.connection.on('error',function (err) {
+  console.log('Mongoose default connection error: ' + err);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose default connection disconnected');
+});
+
+// When the connection is open
+mongoose.connection.on('open', function () {
+  console.log('Mongoose default connection is open');
+});
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', function() {
+  mongoose.connection.close(function () {
+    console.log('Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  });
+});
+
+
+module.exports = mongoose.connection;
--- strankeToXml/stranka.js
+++ strankeToXml/stranka.js
@@ -5,71 +5,64 @@ const fs = require('fs'),
       XLSX = require('xlsx');
       stranke = require('./../data/strankeJSON')
 
-class Stranka{
+class Stranke{
 
   constructor(){
-    this.mapping = "/../mappings/stranka.xml"
-    this.xmlobjmap = null
 
-    this.stranka = {}
+
+    this.objXml = {
+
+           miniMAXUvozKnjigovodstvo:{
+              '$':{ xmlns: 'http://moj.minimax.si/ip/doc/schemas/miniMAXUvozKnjigovodstvo',
+                  'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
+                  'xsi:schemaLocation': 'http://moj.minimax.si/ip/doc/schemas/miniMAXUvozKnjigovodstvo http://moj.minimax.si/ip/doc/schemas/miniMAXUvozKnjigovodstvo.xsd' },
+               Stranke: [
+                 {
+                   Stranka:[]
+                 }
+               ]
+             }
+
+         }
 
 
 
   }
 
-  createObjectsFromMap(cb){
-    fs.readFile(__dirname + this.mapping, (err, data)=> {
-        parser.parseString(data, (err, result)=> {
-            
-            this.stranka = result.Stranka
-            result.Stranka = null
-            this.xmlobjmap = result
-            cb(err)
-        });
-    });
-  }
 
-  addStranka(data){
-    this.xmlobjmap.Stranka = data
+  addStranka(stranka){
+    this.objXml.miniMAXUvozKnjigovodstvo.Stranke[0].Stranka.push(stranka)
   }
 
 
   toString(){
-
-    return builder.buildObject(this.xmlobjmap);
+    return builder.buildObject(this.objXml);
   }
 }
+
+class Stranka{
+  constructor(sifra, naziv, nasolv, kraticaDrzave, postnaStevilka, nazivPoste, davcnaStevilka, identifikacijskaStevilka){
+    this.data = {
+      Sifra: sifra,
+      Naziv: naziv,
+      Naslov: nasolv,
+      KraticaDrzave: kraticaDrzave,
+      PostnaStevilka: postnaStevilka,
+      NazivPoste: nazivPoste,
+      DavcnaStevilka: davcnaStevilka,
+      IdentifikacijskaStevilka: identifikacijskaStevilka,
+    }
+  }
+}
+
 
 //console.log(stranke)
 
 
-module.exports = Stranka
+module.exports = Stranke
 
 
 /*
-//// ISKANJE NALSOVOV STRANK PREKO BAZE PODATKJOV VSEH REGISTRERANIH PODJETJI
-GlobalCustomer = require("./../GlobalCustomer")
-db =  require("./../db")
-var counter = 0
-Object.keys(stranke).forEach((key)=>{
-
-  GlobalCustomer.findOne({vat: stranke[key].ID_DDV},(err, customer)=>{
-    if(customer){
-      stranke[key].KRAJ = customer.city.trim()
-      stranke[key].POSTA = customer.postal_num.trim()
-      stranke[key].NASLOV = customer.address.trim()
-    }else{
-      stranke[key].KRAJ = ""
-      stranke[key].POSTA = ""
-      stranke[key].NASLOV = ""
-    }
-    counter++
-    if(counter === Object.keys(stranke).length)console.log(JSON.stringify(stranke))
-  })
-})
-
-
-
 
 ////////////BRANJE PODATKOV IZ EXELOVE TABELE
 var workbook = XLSX.readFile('../data/stranke.xls');
@@ -105,9 +98,88 @@ var stranke = {}
 data.forEach((stranka)=>{
   stranke[stranka.PARTNER] =  stranka
 })
-console.log(JSON.stringify(stranke))
 
 
+//// ISKANJE NALSOVOV STRANK PREKO BAZE PODATKJOV VSEH REGISTRERANIH PODJETJI
+GlobalCustomer = require("./../GlobalCustomer")
+db =  require("./../db")
+var counter = 0
+Object.keys(stranke).forEach((key)=>{
+
+  GlobalCustomer.findOne({vat: stranke[key].ID_DDV},(err, customer)=>{
+    if(customer){
+      stranke[key].KRAJ = customer.city.trim()
+      stranke[key].POSTA = customer.postal_num.trim()
+      stranke[key].NASLOV = customer.address.trim()
+    }else{
+      stranke[key].KRAJ = ""
+      stranke[key].POSTA = ""
+      stranke[key].NASLOV = ""
+    }
+    counter++
+    if(counter === Object.keys(stranke).length)console.log(JSON.stringify(stranke))
+  })
+})
+*/
+
+
+
+////////////BRANJE PODATKOV IZ EXELOVE TABELE
+var workbook = XLSX.readFile('../data/stranke2.xls');
+var worksheet = workbook.Sheets.Sheet1;
+var headers = {};
+var data = [];
+for(z in worksheet) {
+   if(z[0] === '!') continue;
+   //parse out the column, row, and value
+   var col = z.substring(0,1);
+   var row = parseInt(z.substring(1));
+   var value = worksheet[z].v;
+
+   //store header names
+   if(row == 1) {
+       headers[col] = value;
+       continue;
+   }
+
+   if(!data[row]) data[row]={};
+
+   data[row][headers[col]] = value;
+
+}
+//drop those first two rows which are empty
+data.shift();
+data.shift();
+
+
+
+var strankeXML = new Stranke()
+
+//console.log(data)
+data.forEach((stranka)=>{
+
+  if(stranka.DRZAVA !== "SLOVENIJA" && stranka.DRZAVA){
+
+    var tmp = stranka.POSTA && stranka.POSTA.trim()
+
+    var postna_stevilka = tmp ?(tmp.split(" ").length > 1 ? tmp.split(" ")[0] : "1000"):"1000"
+
+    var posta = tmp ? tmp.replace(postna_stevilka, ""):"NI ZANANO"
+
+
+    var id_ddv = stranka.ID_DDV && Number(stranka.ID_DDV.toString().replace(/\D/g,''))+""
+    if(stranka.lenghth === 8)id_ddv += "0"
+
+
+    var strankaXML = new Stranka(id_ddv, stranka.NAZIV, stranka.NASLOV ? stranka.NASLOV:"NI ZNANO", stranka.DRZAVA_KRATICA, postna_stevilka, posta, id_ddv, id_ddv)
+
+    strankeXML.addStranka(strankaXML.data)
+  }
+
+})
+console.log(strankeXML.toString())
+
+/*
 let xmlobj = new Stranke()
 xmlobj.createObjectsFromMap((err)=>{
   xmlobj.addStranka({
@@ -136,8 +208,8 @@ xmlobj.createObjectsFromMap((err)=>{
   })
   console.log(xmlobj.toString())
 })
-*/
 
+*/
 
 
 /*
